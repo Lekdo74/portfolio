0af14759bc764ec508158fde2b698c5d8f45b45c
Lekdo74 <alexandre.giniaux@etu.univ-smb.fr>
2024-10-01 05:32:20 +0200

    titre desc cv

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+const PDF_URL = "./cv_alexandre_giniaux.pdf";
+
 const css = document.documentElement.style;
+
+var currentCell = null;
+var lastCellHovered = null;
 
 function cssVar(name,value){
     if(name[0]!='-') name = '--'+name
@@ -17,6 +22,9 @@ function initGrid(){
     grid.id = "background-grid";
 
     cells_to_generate = Math.ceil(window.screen.width / parseInt(cssVar("cell-width").slice(0, 2)) * window.screen.height / parseInt(cssVar("cell-height").slice(0, 2)))
+    
+    //il faut en générer plus puisque le grille est penchée pour que ça recouvre tout l'écran
+    cells_to_generate *= 4
 
     for (let i = 0; i < cells_to_generate; i++) {
         addCell(grid);
@@ -29,4 +37,45 @@ function addCell(container){
     return cell;
 }
 
+function initVignette(){
+    vignette = createEl("div", document.body);
+    vignette.id = "vignette";
+}
+
+function randomCellHoverColor(){
+    if(currentCell === lastCellHovered){
+        return;
+    }
+
+    currentCell = lastCellHovered;
+
+    random_number = Math.floor(Math.random() * 2);
+
+    if(random_number === 0){
+        cssVar("cell-hover-color", "rgba(128, 0, 129, 1)");
+        return;
+    }
+
+    cssVar("cell-hover-color", "rgba(75, 75, 255, 1)");
+    return;
+}
+
+document.addEventListener('mousemove', (event) => {
+    const hoveredElement = document.elementFromPoint(event.clientX, event.clientY);
+    if (hoveredElement) {
+        lastCellHovered = hoveredElement;
+        randomCellHoverColor();
+    }
+});
+
+function initCV(){
+    cv = document.getElementById("cv");
+    
+    cv.addEventListener("click", () => {
+        window.open(PDF_URL, '_blank');
+    })
+}
+
 initGrid();
+initVignette();
+initCV();
