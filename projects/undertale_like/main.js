29917c0a083ac11e22c3b67d319e0818b948ef3c
Lekdo74 <alexandre.giniaux@etu.univ-smb.fr>
2024-10-03 18:12:52 +0200

    jump ReleasedJumpKeySinceLastJump

--- projects/undertale_like/main.js
+++ projects/undertale_like/main.js
@@ -1,6 +1,7 @@
 const TARGETED_GAMELOOP_FREQUENCY = 1 / 60
 
 const PLAYER_SPEED = 50;
+const PLAYER_MAX_JUMP_HOLD_TIME = 1;
 const PLAYER_JUMP_FORCE = 1200;
 const PLAYER_RELEASE_JUMP_FORCE = 400;
 
@@ -18,16 +19,7 @@ window.onkeydown = function (e) { pressedKeys[e.keyCode] = true; }
 function Classes(bases) {
     class Bases {
         constructor() {
-            bases.forEach(base => {
-                const instance = new base();
-                // Object.assign(this, instance);
-
-                Object.getOwnPropertyNames(instance).forEach(prop => {
-                    if (prop.startsWith('#')) {
-                        console.log("test")
-                    }
-                });
-            });
+            bases.forEach(base => Object.assign(this, new base()));
         }
     }
 
@@ -81,9 +73,13 @@ class WidthHeight {
 }
 
 class Player {
-    constructor(heart){
+    constructor(heart) {
         this._heart = heart;
         this._yVelocity = 0;
+        this._jumpHoldTime = 0;
+        this.Grounded = false;
+        this._canJump = false;
+        this._releasedJumpKeySinceLastJump = true;
     }
 
     get Heart() {
@@ -98,6 +94,34 @@ class Player {
     }
     set YVelocity(value) {
         this._yVelocity = value;
+    }
+
+    get JumpHoldTime() {
+        return this._jumpHoldTime;
+    }
+    set JumpHoldTime(value) {
+        this._jumpHoldTime = value;
+    }
+
+    get Grounded(){
+        return this._grounded;
+    }
+    set Grounded(value) {
+        this._grounded = value;
+    }
+
+    get CanJump() {
+        return this._canJump;
+    }
+    set CanJump(value) {
+        this._canJump = value;
+    }
+
+    get ReleasedJumpKeySinceLastJump() {
+        return this._releasedJumpKeySinceLastJump;
+    }
+    set ReleasedJumpKeySinceLastJump(value) {
+        this._releasedJumpKeySinceLastJump = value;
     }
 }
 
@@ -158,8 +182,8 @@ class Box extends Classes([Coordinates, WidthHeight]) {
         this.div = createEl("div", document.body);
         this.div.classList.add("box");
 
-        this.Width = 350;
-        this.Height = 350;
+        this.Width = 330;
+        this.Height = 330;
         this.X = (window.innerWidth - this.Width) / 2;
         this.Y = (window.innerHeight - this.Height) / 2;
     }
@@ -214,6 +238,8 @@ function createEl(tag, container) {
 
 function applyGravity(deltaTime) {
     player.YVelocity -= GRAVITY_FORCE * deltaTime;
+
+    player.Heart.Y -= player.YVelocity * deltaTime;
 }
 
 function move(deltaTime) {
@@ -221,23 +247,42 @@ function move(deltaTime) {
 
     let vector = [0, 0];
 
-    keyup: if (keyUp()) {
-        if(movementType === MovementType.ADJUMP){
-            player.YVelocity = PLAYER_JUMP_FORCE * deltaTime;
-            break keyup;
+    if (keyUp()) {
+        if (movementType === MovementType.ADJUMP) {
+            if (player.CanJump && player.ReleasedJumpKeySinceLastJump && player.JumpHoldTime < PLAYER_MAX_JUMP_HOLD_TIME) {
+                player.Grounded = false;
+                player.JumpHoldTime += deltaTime;
+                player.YVelocity = PLAYER_JUMP_FORCE * deltaTime;
+            }
+            else{
+                player.ReleasedJumpKeySinceLastJump = false;
+            }
         }
-        vector[1] = -1;
+        else {
+            vector[1] = -1;
+        }
     }
-    else{
-        if(movementType === MovementType.ADJUMP && player.YVelocity > PLAYER_RELEASE_JUMP_FORCE * deltaTime){
-            player.YVelocity = PLAYER_RELEASE_JUMP_FORCE * deltaTime;
+    else {
+        if (movementType === MovementType.ADJUMP) {
+            player.ReleasedJumpKeySinceLastJump = true;
+
+            if(!player.Grounded){
+                player.CanJump = false;
+                player.JumpHoldTime = 0;
+            }
+
+            if(player.YVelocity > PLAYER_RELEASE_JUMP_FORCE * deltaTime){
+                player.YVelocity = PLAYER_RELEASE_JUMP_FORCE * deltaTime;
+            }
         }
     }
-    keydown: if (keyDown()) {
-        if(movementType === MovementType.ADJUMP){
-            break keydown;
+    if (keyDown()) {
+        if (movementType === MovementType.ADJUMP) {
+            
         }
-        vector[1] = 1;
+        else {
+            vector[1] = 1;
+        }
     }
     if (keyRight()) {
         vector[0] = 1;
@@ -247,19 +292,13 @@ function move(deltaTime) {
     }
 
     let magnitude = Math.sqrt(vector[0] ** 2 + vector[1] ** 2);
-    if(magnitude === 0){
+    if (magnitude === 0) {
         return;
     }
     let normalizedVector = [vector[0] / magnitude, vector[1] / magnitude];
 
     heart.X += normalizedVector[0] * PLAYER_SPEED * deltaTime;
     heart.Y += normalizedVector[1] * PLAYER_SPEED * deltaTime;
-}
-
-function applyJump(deltaTime){
-    let heart = player.Heart;
-
-    heart.Y -= player.YVelocity * deltaTime;
 }
 
 function forceInBox() {
@@ -279,6 +318,9 @@ function forceInBox() {
         heart.Y = upperBoundary;
     }
     if (y2 > bottomBoundary) {
+        player.Grounded = true;
+        player.CanJump = true;
+        player.JumpHoldTime = 0;
         heart.Y = bottomBoundary - heart.Height;
     }
     if (x1 < leftBoundary) {
@@ -315,9 +357,12 @@ function gameLoop() {
 
         applyGravity(deltaTime);
         move(deltaTime);
-        applyJump(deltaTime);
         forceInBox();
         // console.log(checkCollisions());
+
+        // console.log(["grounded", player.Grounded]);
+        // console.log(["can jump", player.CanJump]);
+        // console.log(["ReleasedJumpKeySinceLastJump", player.ReleasedJumpKeySinceLastJump]);
 
         lastUpTime = performance.now();
     }, TARGETED_GAMELOOP_FREQUENCY)
@@ -325,7 +370,7 @@ function gameLoop() {
 
 function initGame() {
     box = new Box();
-    player = new  Player(new Heart());
+    player = new Player(new Heart());
     movementType = MovementType.ADJUMP;
     gameLoop()
 }
