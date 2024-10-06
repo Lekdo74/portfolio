71c71030d67f79bc4f519c2f8b65f5b012736d66
Lekdo74 <alexandre.giniaux@etu.univ-smb.fr>
2024-10-06 06:41:35 +0200

    joker

--- projects/undertale_like/main.js
+++ projects/undertale_like/main.js
@@ -1,15 +1,23 @@
 const TARGETED_GAMELOOP_FREQUENCY = 1 / 60
 
+const PLAYER_MAX_HEALTH = 50;
 const PLAYER_SPEED = 50;
 const PLAYER_MAX_JUMP_HOLD_TIME = 1;
 const PLAYER_JUMP_FORCE = 1200;
 const PLAYER_RELEASE_JUMP_FORCE = 400;
+const PLAYER_HEALTHBAR_HEIGHT = 30;
+
+const NUMBER_OF_CARDS = 52;
+const CARD_WIDTH = 54;
+const CARD_HEIGHT = CARD_WIDTH / 27 * 34;
 
 const GRAVITY_FORCE = 50;
 
 let lastUpTime = performance.now();
 let movementType;
 
+let currentStage;
+let stateFinished = false;
 
 var pressedKeys = {};
 window.onkeyup = function (e) { pressedKeys[e.keyCode] = false; }
@@ -83,12 +91,20 @@ class WidthHeight {
 
 class Player {
     constructor(heart) {
+        this._health = PLAYER_MAX_HEALTH;
         this._heart = heart;
         this._yVelocity = 0;
         this._jumpHoldTime = 0;
         this.Grounded = false;
         this._canJump = false;
         this._releasedJumpKeySinceLastJump = true;
+    }
+
+    get Health() {
+        return this._health;
+    }
+    set Health(value) {
+        this._health = value;
     }
 
     get Heart() {
@@ -141,7 +157,7 @@ class Obstacle {
 }
 
 class Rectangle extends Classes([Coordinates, WidthHeight]) {
-    constructor(x, y, w, h, debug = false) {
+    constructor(x = 0, y = 0, w = 0, h = 0, debug = false) {
         super();
 
         if (debug) {
@@ -201,9 +217,6 @@ class Heart extends Classes([Coordinates, WidthHeight]) {
             }
         }
 
-        this.X = (window.innerWidth - this.Width) / 2;
-        this.Y = (window.innerHeight - this.Height) / 2;
-
         let rectanglesData = {};
         for (let i = 0; i < heartData.length; i++) {
             for (let j = 0; j < heartData[0].length; j++) {
@@ -259,6 +272,281 @@ class Box extends Classes([Coordinates, WidthHeight]) {
     }
 }
 
+class Boss extends Classes([Coordinates, WidthHeight]) {
+    constructor() {
+        super();
+
+        this.div = createEl("img", document.body);
+        this.div.src = "./images/joker/joker_smiling.png";
+        this.div.classList.add("boss");
+
+        //image en 56x75
+        this.Width = 200;
+        this.Height = this.Width / 56 * 75;
+    }
+}
+
+class Deck extends Coordinates {
+    constructor() {
+        super();
+
+        this.div = createEl("div", document.body);
+        this.div.classList.add("deck");
+
+        this._cards = [];
+        for(let i = 0; i < NUMBER_OF_CARDS; i++){
+            let card = new Card(this.div);
+            card.div.style.transform = "rotate(" + biasedRandom(-6, 6, 2) + "deg) translate(" + biasedRandom(-20, 20, 10) + "%, " + biasedRandom(-20, 20, 10) + "%)";
+            
+            this._cards.push(card);
+        }
+    }
+
+    get Cards() {
+        return this._cards;
+    }
+    set Cards(value) {
+        this._cards = value;
+    }
+}
+
+class Card extends Classes([Coordinates, WidthHeight]) {
+    constructor(container = document.body) {
+        super();
+
+        this.div = createEl("img", container);
+        this.div.src = "./images/cards/back.png";
+        this.div.classList.add("card");
+
+        //image en 27x34
+        this.Width = CARD_WIDTH;
+        this.Height = CARD_HEIGHT;
+    }
+}
+
+class HealthBar extends Classes([Coordinates, WidthHeight]) {
+    constructor() {
+        super();
+
+        this.div = createEl("div", document.body);
+        this.div.classList.add("health-bar");
+
+        this.Width = PLAYER_MAX_HEALTH * 3;
+        this.Height = PLAYER_HEALTHBAR_HEIGHT;
+
+        this.Back = new Rectangle();
+        this.Front = new Rectangle();
+
+        this.TextHp = createEl("div", this.div);  
+        this.TextHp.classList.add("health-bar-text");
+        this.TextHp.style.marginTop = "3px";
+        this.TextHp.innerHTML = "HP";
+
+        this.Bars = createEl("div", this.div);
+        this.Bars.classList.add("health-bar-bars");
+
+        this.Back.div = createEl("div", this.Bars);
+        this.Back.div.classList.add("health-bar-back");
+        this.Back.Width = this.Width;
+        this.Back.Height = this.Height;
+
+        this.Front.div = createEl("div", this.Bars);
+        this.Front.div.classList.add("health-bar-front");
+        this.Front.Width = this.Width;
+        this.Front.Height = this.Height;
+
+        this.TextHpValue = createEl("div", this.div);  
+        this.TextHpValue.classList.add("health-bar-text");
+        this.TextHpValue.style.fontSize = "20px";
+        this.TextHpValue.style.marginTop = "3px";
+        this.TextHpValue.innerHTML = PLAYER_MAX_HEALTH + "/" + PLAYER_MAX_HEALTH;
+    }
+
+    changeHealth(health){
+        this.Front.Width = health * 3;
+        this.TextHpValue.innerHTML = health + "/" + PLAYER_MAX_HEALTH;
+    }
+
+    get Back() {
+        return this._back;
+    }
+    set Back(value) {
+        this._back = value;
+    }
+
+    get Front() {
+        return this._front;
+    }
+    set Front(value) {
+        this._front = value;
+    }
+
+    get Bars() {
+        return this._bars;
+    }
+    set Bars(value) {
+        this._bars = value;
+    }
+
+    get TextHp() {
+        return this._textHp;
+    }
+    set TextHp(value) {
+        this._textHp = value;
+    }
+
+    get TextHpValue() {
+        return this._textHpValue;
+    }
+    set TextHpValue(value) {
+        this._textHpValue = value;
+    }
+}
+
+class Stage {
+    constructor(entryEvent, events, nextStageActivation) {
+        this._started = false;
+        this._entryEvent = entryEvent;
+        this._events = events;
+        this._nextStageActivation = nextStageActivation;
+        this._finished = false;
+    }
+
+    start(){
+        this.Started = true;
+        this.EntryEvent.start();
+    }
+
+    update(){
+        if(this.EntryEvent.OnGoing === true){
+            return;
+        }
+
+        let eventsOnGoing = false;
+
+        for(let i = 0; i < this.Events.length; i++){
+            const event = this.Events[i];
+
+            if(event.OnGoing === true){
+                eventsOnGoing = true;
+                break;
+            }
+        }
+
+        if(this.NextStageActivation() && !eventsOnGoing){
+            this._finished = true;
+            return;
+        }
+
+        if(eventsOnGoing === false){
+            for(let i = 0; i < this.Events.length; i++){
+                const event = this.Events[i];
+
+                event.start();
+            }
+        }
+    }
+
+    get Started() {
+        return this._started;
+    }
+    set Started(value) {
+        this._started = value;
+    }
+
+    get EntryEvent() {
+        return this._entryEvent;
+    }
+    set EntryEvent(value) {
+        this._entryEvent = value;
+    }
+
+    get Events() {
+        return this._events;
+    }
+    set Events(value) {
+        this._events = value;
+    }
+
+    get NextStageActivation() {
+        return this._nextStageActivation;
+    }
+    set NextStageActivation(value) {
+        this._nextStageActivation = value;
+    }
+
+    get Finished() {
+        return this._finished;
+    }
+    set Finished(value) {
+        this._finished = value;
+    }
+}
+
+class Attack {
+    get Cards() {
+        return this._cards;
+    }
+    set Cards(value) {
+        this._cards = value;
+    }
+
+    get OnGoing() {
+        return this._onGoing;
+    }
+    set OnGoing(value) {
+        this._onGoing = value;
+    }
+}
+
+class Dialog extends Attack {
+    constructor() {
+        super();
+    }
+
+    start(){
+        this.OnGoing = true;
+        console.log("dialog started");
+
+        setTimeout(() => {
+            this.OnGoing = false;
+            console.log("dialog finished");
+        }, 3000);
+    }
+}
+
+class AttackOne extends Attack {
+    constructor() {
+        super();
+    }
+
+    start(){
+        this.OnGoing = true;
+        console.log("attack one started");
+
+        setTimeout(() => {
+            this.OnGoing = false;
+            console.log("attack one finished");
+        }, 3000);
+    }
+}
+
+class AttackTwo extends Attack {
+    constructor() {
+        super();
+    }
+
+    start(){
+        this.OnGoing = true;
+        console.log("attack two started");
+
+        setTimeout(() => {
+            this.OnGoing = false;
+            console.log("attack two finished");
+        }, 3000);
+    }
+}
+
 class MovementType {
     static #WASD = 0;
     static #ADJUMP = 1;
@@ -267,11 +555,25 @@ class MovementType {
     static get ADJUMP() { return this.#ADJUMP; }
 }
 
+class State {
+    static #DIALOG = 0;
+    static #DRAWINGCARDS = 1;
+    static #ATTACK = 2;
+
+    static get DIALOG() { return this.#DIALOG; }
+    static get DRAWINGCARDS() { return this.#DRAWINGCARDS; }
+    static get ATTACK() { return this.#ATTACK; }
+}
+
+let stages;
 let box;
 let player;
+let healthBar;
+let boss;
+let deck;
 let obstacles = [];
-let obstacl = new Obstacle(new Rectangle(window.innerWidth / 2 + 50, window.innerHeight / 2 + 50, 100, 100, true));
-obstacles.push(obstacl);
+// let obstacl = new Obstacle(new Rectangle(window.innerWidth / 2 + 50, window.innerHeight / 2 + 50, 100, 100, true));
+// obstacles.push(obstacl);
 
 function cssVar(name, value) {
     if (name[0] != '-') name = '--' + name;
@@ -310,7 +612,7 @@ function move(deltaTime) {
             }
         }
         else {
-            vector[1] = -1;
+            vector[1] -= 1;
         }
     }
     else {
@@ -332,14 +634,14 @@ function move(deltaTime) {
 
         }
         else {
-            vector[1] = 1;
+            vector[1] += 1;
         }
     }
     if (keyRight()) {
-        vector[0] = 1;
+        vector[0] += 1;
     }
     if (keyLeft()) {
-        vector[0] = -1;
+        vector[0] -= 1;
     }
 
     let magnitude = Math.sqrt(vector[0] ** 2 + vector[1] ** 2);
@@ -428,6 +730,16 @@ function rectangleCollision(r1, r2) {
     return false;
 }
 
+function drawCards(number){
+    let drawedCards = [];
+
+    for(let i = 0; i < number; i++){
+        drawedCards.push(deck.Cards.pop());
+    }
+
+    return(drawedCards);
+}
+
 function gameLoop() {
     setInterval(() => {
         deltaTime = 1 / (performance.now() - lastUpTime);
@@ -435,21 +747,67 @@ function gameLoop() {
         applyGravity(deltaTime);
         move(deltaTime);
         forceInBox();
-        console.log(checkCollisions());
-
-        // console.log(["grounded", player.Grounded]);
-        // console.log(["can jump", player.CanJump]);
-        // console.log(["ReleasedJumpKeySinceLastJump", player.ReleasedJumpKeySinceLastJump]);
+        
+        const stage = stages[currentStage];
+        if(!stage._started){
+            stage.start();
+        }
+        stage.update();
+        if(stage._finished){
+            if(currentStage + 2 >  stages.length){
+                console.log("End");
+            }
+            else{
+                currentStage++;
+            }
+        }
 
         lastUpTime = performance.now();
     }, TARGETED_GAMELOOP_FREQUENCY)
 }
 
+function initPositions() {
+    const yGlobalOffset = 80;
+    const yUpperBoardOffset = 40;
+    const yLowerBoardOffset = 30;
+
+    box.X = (window.innerWidth - box.Width) / 2;
+    box.Y = (window.innerHeight - box.Height) / 2 + yGlobalOffset;
+
+    player.Heart.X = (window.innerWidth - player.Heart.Width) / 2;
+    player.Heart.Y = (window.innerHeight - player.Heart.Height) / 2 + yGlobalOffset;
+
+    healthBar.Width = box.Width
+    healthBar.X = (window.innerWidth - healthBar.Width) / 2;
+    healthBar.Y = (window.innerHeight + box.Height) / 2 + yLowerBoardOffset + yGlobalOffset;
+
+    boss.X = (window.innerWidth - boss.Width) / 2;
+    boss.Y = (window.innerHeight - box.Height) / 2 - boss.Height - yUpperBoardOffset + yGlobalOffset;
+
+    deck.X = window.innerWidth / 2 + box.Width / 2 + 50;
+    deck.Y = (window.innerHeight - box.Height) / 2 - CARD_HEIGHT - yUpperBoardOffset + yGlobalOffset - 20;
+}
+
 function initGame() {
+    console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")//clears console
+
+    stages = [
+        new Stage(new Dialog(), [new AttackOne()], () => { return player.Health <= 40 }),
+        new Stage(new Dialog(), [new AttackTwo()], () => { return player.Health <= 30 }),
+    ]
+    currentStage = 0;
+
     box = new Box();
     player = new Player(new Heart());
-    movementType = MovementType.ADJUMP;
-    gameLoop()
+    healthBar = new HealthBar();
+    boss = new Boss();
+    deck = new Deck();
+    initPositions();
+    movementType = MovementType.WASD;
+
+
+
+    gameLoop();
 }
 
 initGame()
@@ -480,6 +838,20 @@ function keyLeft() {
     return false;
 }
 
+//returns a number between min and max but more likely to be in the middle than at the extremes depending on the power
+function biasedRandom(min, max, power = 2) {
+    // Generate a random number between 0 and 1
+    let random = Math.random();
+    
+    // Square the random value to skew it towards 0
+    random = random ** power;
+    
+    if (Math.random() < 0.5) {
+        random = -random;
+    }
+    
+    return random * (max - min) / 2 + (min + max) / 2;
+}
 
 // document.addEventListener("mousemove", (event) => {
 //     console.log([event.clientY])
